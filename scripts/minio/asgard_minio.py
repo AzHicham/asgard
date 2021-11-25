66edbd535a579e44960f86092aa29c07ed9d6488
xlqian <patrick.qian@kisio.com>
2021-11-25 16:49:02 +0100

    refacto; download specific files

---
+++ scripts/minio/asgard_minio.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" A MinIO Downloader dedicated to download Valhalla data
+
+Usage:
+  asgard_minio.py (-h | --help)
+  asgard_minio.py download [-k S3_KEY | --key=S3_KEY] [-s S3_SECRET | --secret=S3_SECRET] [-H HOST | --host=HOST] [-F] [-b BUCKET | --bucket=BUCKET] [-c COVERAGE | --coverage=COVERAGE] [-V VALHALLA_VERSION | --valhalla_version=VALHALLA_VERSION]
+  asgard_minio.py upload <files>... [-k S3_KEY | --key=S3_KEY] [-s S3_SECRET | --secret=S3_SECRET] [-H HOST | --host=HOST] [-F] [-b BUCKET | --bucket=BUCKET] [-c COVERAGE | --coverage=COVERAGE] [-V VALHALLA_VERSION | --valhalla_version=VALHALLA_VERSION]
+
+Options:
+  -h --help                                                     Show this screen.
+  -k S3_KEY, --key=S3_KEY                                       Minio/S3 key/username
+  -s S3_SECRET, --secret=S3_SECRET                              Minio/S3 secret/token
+  -H HOST, --host=HOST                                          Minio/S3 hostname
+  -b BUCKET, --bucket=BUCKET                                    Minio/S3 Bucket's name
+  -c COVERAGE, --coverage=COVERAGE                              OSM/data_set name
+  -F --force                                                    Force download data even if present locally
+  -V VALHALLA_VERSION, --valhalla_version=VALHALLA_VERSION      Valhalla version used to build Asgard
+
+Example:
+  asgard_minio.py download -s minioadmin -k minioadmin -H 127.0.0.1:9000 -b asgard -c "europe" -V "3.1.2"
+  asgard_minio.py upload toto.py titi.py -s minioadmin -k minioadmin -H 127.0.0.1:9000 -b asgard -c "europe" -V "3.1.2"
+
+"""
+
+import os.path
+import glob
+from collections import OrderedDict
+from datetime import datetime
+from parse import *
+from minio import Minio
+from minio.error import MinioException
+from minio_progress import Progress
+from minio.commonconfig import Tags
+from minio_common import *
+from minio_download import download
+from minio_upload import upload
+import minio_config
+
+
+def parse_args():
+    from docopt import docopt
+    return docopt(__doc__, version='Asgard Minio Data Management v0.0.1')
+
+
+def main():
+    args = parse_args()
+    config = minio_config.Config(args)
+    check_config(config)
+
+    if args.get('download'):
+        download(config)
+
+    if args.get('upload'):
+        upload(config, args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/minio/minio_config.py
@@ -0,0 +1,24 @@
+# [-k S3_KEY | --key=S3_KEY] [-s S3_SECRET | --secret=S3_SECRET] [-H HOST | --host=HOST] [-F] [-b BUCKET | --bucket=BUCKET] [-c COVERAGE | --coverage=COVERAGE] [-V VALHALLA_VERSION | --valhalla_version=VALHALLA_VERSION]
+from dataclasses import dataclass
+import os
+
+
+@dataclass
+class Config:
+    key: str = os.getenv('MINIO_KEY', "key")
+    secret: str = os.getenv('MINIO_SECRET', "secret")
+    host: str = os.getenv('MINIO_HOST', "localhost:9000")
+    bucket: str = os.getenv('MINIO_BUCKET', "asgard")
+    coverage: str = os.getenv('MINIO_COVERAGE', "europe")
+    valhalla_version: str = os.getenv('MINIO_VALHALLA_VERSION', "3.1.2")
+    force: bool = False
+
+    def __init__(self, args=None):
+        args = args or {}
+        self.key = args.get('--bucket') or self.key
+        self.secret = args.get('--secret') or self.secret
+        self.host = args.get('--host') or self.host
+        self.bucket = args.get('--bucket') or self.bucket
+        self.coverage = args.get('--coverage') or self.coverage
+        self.valhalla_version = args.get('--valhalla_version') or self.valhalla_version
+        self.force = args.get('--force') or self.force
